7aebe319a2dc1e91b1e3b67d81a94c47aa4e2926
Lauriethefish <lauriethefish@outlook.com>
2021-03-30 16:35:55 +0100

    Copy module.json

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -10,5 +10,5 @@ $ArchiveName = "discord-presence_v0.3.1.qmod"
 $TempArchiveName = "discord-presence_v0.3.1.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/libdiscord-presence.so", "./libs/arm64-v8a/libbeatsaber-hook_1_2_4.so", "./mod.json" -DestinationPath $TempArchiveName -Force
+Compress-Archive -Path "./libs/arm64-v8a/libdiscord-presence.so", "./libs/arm64-v8a/libbeatsaber-hook_1_2_4.so", "./mod.json", "./module.json" -DestinationPath $TempArchiveName -Force
 Move-Item $TempArchiveName $ArchiveName -Force
