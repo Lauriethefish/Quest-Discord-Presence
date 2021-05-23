79ed9f7fefc50300124f18d7d490cb50e17fc795
Lauriethefish <lauriethefish@outlook.com>
2021-05-23 17:32:57 +0100

    Version bump dependencies

--- Android.mk
+++ Android.mk
@@ -20,18 +20,20 @@ rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 
 # Creating prebuilt for dependency: beatsaber-hook - version: 1.2.4
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_1_2_4
+# Creating prebuilt for dependency: beatsaber-hook - version: 1.3.5
+include $(CLEAR_VARS)
+LOCAL_MODULE := beatsaber-hook_1_3_5
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_2_4.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_3_5.so
 LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: codegen - version: 0.7.0
+# Creating prebuilt for dependency: codegen - version: 0.8.1
 include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_7_0
+LOCAL_MODULE := codegen_0_8_1
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_7_0.so
+LOCAL_SRC_FILES := extern/libcodegen_0_8_1.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: modloader - version: 1.0.4
+# Creating prebuilt for dependency: modloader - version: 1.1.0
 include $(CLEAR_VARS)
 LOCAL_MODULE := modloader
 LOCAL_EXPORT_C_INCLUDES := extern/modloader
@@ -43,9 +45,9 @@ LOCAL_MODULE := discord-presence
 LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_2_4
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_3_5
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += codegen_0_7_0
+LOCAL_SHARED_LIBRARIES += codegen_0_8_1
 LOCAL_LDLIBS += -llog
 LOCAL_CFLAGS += -I'extern/libil2cpp/il2cpp/libil2cpp' -I'extern/codegen/include' -DID='"discord-presence"' -DVERSION='"0.3.1"' -I'./shared' -I'./extern'
 LOCAL_CPPFLAGS += -std=c++2a
